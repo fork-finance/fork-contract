f985c6a54ff74c25e59013d60cc60d733f331a27
jerrkill <2273716951@qq.com>
2021-03-15 01:03:31 +0800

    fix: UniswapV2Router02 deploy miss address of WETH in testnet

--- migrations/03_deploy_pancakeswap.js
+++ migrations/03_deploy_pancakeswap.js
@@ -1,12 +1,16 @@
 const UniswapV2Factory = artifacts.require('UniswapV2Factory');
 const UniswapV2Router02 = artifacts.require('UniswapV2Router02');
+const MockWBNB = artifacts.require('MockWBNB');
 
 module.exports = async (deployer, network, accounts) => {
     if (network != 'mainnet') {
+        await deployer.deploy(MockWBNB);
+        const wbnb = await MockWBNB.deployed();
+
         console.log('Deploying pancakeswap on '+network+' network.');
-        await deployer.deploy(UniswapV2Factory, accounts[0]);
+       await deployer.deploy(UniswapV2Factory, accounts[0]);
         const uniswapFactory = await UniswapV2Factory.deployed();
-        await deployer.deploy(UniswapV2Router02, uniswapFactory.address, accounts[0]);
+        await deployer.deploy(UniswapV2Router02, uniswapFactory.address, wbnb.address);
     }
     
 };
