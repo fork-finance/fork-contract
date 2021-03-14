2338cd7a2981011baab0c91b8cf56a4a031e7263
jerrkill <2273716951@qq.com>
2021-03-14 15:59:35 +0800

    ci: Change some config in truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -47,8 +47,6 @@ module.exports = {
       host: '127.0.0.1', // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
       network_id: '57777',
-      gasPrice: 50000000000,
-      gas: 6721975,
     },
 
     ganache: {
@@ -133,13 +131,28 @@ module.exports = {
     solc: {
       version: "0.6.6",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1,
+      },
+      evmVersion: "istanbul",
+      outputSelection: {
+        "*": {
+          "": [
+            "ast"
+          ],
+          "*": [
+            "evm.bytecode.object",
+            "evm.deployedBytecode.object",
+            "abi",
+            "evm.bytecode.sourceMap",
+            "evm.deployedBytecode.sourceMap",
+            "metadata"
+          ]
+        }
+      },
+    },
     }
   }
 };
