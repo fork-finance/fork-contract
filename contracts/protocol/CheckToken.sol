8e831253c9c77e53054d8c686e83f568843cde90
jerrkill <2273716951@qq.com>
2021-04-06 20:34:54 +0800

    style: Remove some style code

--- contracts/protocol/CheckToken.sol
+++ contracts/protocol/CheckToken.sol
@@ -2,24 +2,14 @@ pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // CheckToken with Governance.
 contract CheckToken is ERC20("CheckToken", "CHECK"), Ownable {
 
-  // uint256 public manualMintLimit = 8000000e18;
-  uint256 public manualMintLimit = 250000e18;
-  uint256 public manualMinted = 0;
-
   constructor() public {
     _setupDecimals(18);
     // maunalMint 250k for seeding liquidity
-    manualMint(msg.sender, 250000e18);
-  }
-
-  function manualMint(address _to, uint256 _amount) public onlyOwner {
-    require(manualMinted <= manualMintLimit, "mint limit exceeded");
-    mint(_to, _amount);
+    mint(msg.sender, 250000e18);
   }
 
   function mint(address _to, uint256 _amount) public onlyOwner {
@@ -33,5 +23,4 @@ contract CheckToken is ERC20("CheckToken", "CHECK"), Ownable {
   function transferAll(address _to) public {
     _transfer(msg.sender, _to, balanceOf(msg.sender));
   }
-  
 }
