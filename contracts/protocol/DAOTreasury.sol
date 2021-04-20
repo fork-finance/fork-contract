603c0862457781ad3aacee750a1e3206214c31b1
jerrkill <2273716951@qq.com>
2021-04-21 01:24:47 +0800

    feat: DAOThreasury and DevShare

---
+++ contracts/protocol/DAOTreasury.sol
@@ -0,0 +1,36 @@
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DAOTreasury is Ownable {
+  using SafeMath for uint256;
+  using SafeERC20 for IERC20;
+
+  string public constant name = "Fork Finance DAO Treasury";
+
+  uint256 public startReleaseBlock;
+  uint256 public endReleaseBlock;
+
+  IERC20 public fork;
+
+  constructor(address _fork) public {
+    fork = IERC20(_fork);
+  }
+
+  function transferForkTo(
+    address _to,
+    uint256 _amount
+  ) external onlyOwner {
+    fork.safeTransfer(_to, _amount);
+  }
+
+  function transferAnyTo(
+    address _token,
+    address _to,
+    uint256 _amount
+  ) external onlyOwner {
+    IERC20(_token).safeTransfer(_to, _amount);
+  }
+}
---
+++ contracts/protocol/DevShare.sol
@@ -0,0 +1,57 @@
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DevShare is Ownable {
+  using SafeMath for uint256;
+  using SafeERC20 for IERC20;
+
+  string public constant name = "Fork Finance Dev Share";
+
+  IERC20 public fork;
+
+  uint256 public startReleaseBlock;
+  uint256 public endReleaseBlock;
+  uint256 public lastUnlockBlock;
+
+  constructor(address _fork, uint256 _startReleaseBlock, uint256 _endReleaseBlock) public {
+    require(_endReleaseBlock > _startReleaseBlock, "endReleaseBlock < startReleaseBlock");
+    fork = IERC20(_fork);
+    startReleaseBlock = _startReleaseBlock;
+    endReleaseBlock = _endReleaseBlock;
+    lastUnlockBlock = _startReleaseBlock;
+  }
+
+  function canUnlockAmount() public view returns (uint256) {
+    uint256 bal = fork.balanceOf(address(this));
+    // When block number less than startReleaseBlock, no FORKs can be unlocked
+    if (block.number < startReleaseBlock) {
+      return 0;
+    }
+    // When block number more than endReleaseBlock, all locked FORKs can be unlocked
+    else if (block.number >= endReleaseBlock) {
+      return bal;
+    }
+    // When block number is more than startReleaseBlock but less than endReleaseBlock,
+    // some FORKs can be released
+    else
+    {
+      uint256 releasedBlock = block.number.sub(lastUnlockBlock);
+      uint256 blockLeft = endReleaseBlock.sub(lastUnlockBlock);
+      return bal.mul(releasedBlock).div(blockLeft);
+    }
+  }
+
+  function unlock(address _to) public {
+    uint256 bal = fork.balanceOf(address(this));
+    require(bal > 0, "no locked FORKs");
+
+    uint256 amount = canUnlockAmount();
+    require(bal >= amount, "wtf not enough fork");
+
+    fork.safeTransfer(_to, amount);
+    lastUnlockBlock = block.number;
+  }
+}
